6e759190330880dbb95f90b501b6db5b8c885fa7
andrestorres04 <61821537+andrestorres04@users.noreply.github.com>
2021-03-05 13:19:50 +0100

    Add files via upload

---
+++ ProyectoInterfaces-main/app/Main.java
@@ -0,0 +1,13 @@
+package app;
+
+import view.LoginView;
+
+public class Main {
+
+	public static void main(String[] args) {
+		LoginView window = new LoginView();
+		window.getFrame().setVisible(true);
+
+	}
+
+}
